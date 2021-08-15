593224fe6a2d41773ceff0766ec07f3ef56cd78a
Anders Damgaard <akd@akd.io>
2021-08-16 01:52:51 +0200

    Add OpenGraph meta tags and thumbnail

--- components/Page.tsx
+++ components/Page.tsx
@@ -5,13 +5,25 @@ import WithDefaultGlobalStyles from "./WithDefaultGlobalStyles";
 type PageProps = {
   title: string;
   description: string;
+  ogImageUrl: string;
 };
-const Page: React.FC<PageProps> = ({ title, description, children }) => {
+const Page: React.FC<PageProps> = ({
+  title,
+  description,
+  ogImageUrl,
+  children,
+}) => {
   return (
     <WithDefaultGlobalStyles>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={ogImageUrl} />
+
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono&display=swap"
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,16 @@ import { NextPage } from "next";
 import Page from "../components/Page";
 import LandingPageTemplate from "../templates/LandingPage/LandingPageTemplate";
 
+const title = "Create your Next.js project today! | Create Next Stack";
+const description =
+  "Create your Next.js project today! Create Next Stack is a website and CLI tool used to easily set up the boilerplate of new Next.js apps.";
+
 const Index: NextPage = () => {
   return (
     <Page
-      title="Create Next Stack"
-      description="Create your Next.js project today! Create Next Stack is a website and CLI tool used to easily set up the boilerplate of new Next.js apps."
+      title={title}
+      description={description}
+      ogImageUrl="https://www.create-next-stack.com/thumbnail.png"
     >
       <LandingPageTemplate />
     </Page>
