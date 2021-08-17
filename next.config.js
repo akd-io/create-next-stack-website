f97a6e80625f182cf35977b78e563c72991267cc
Anders Kjær Damgaard <akd@akd.io>
2021-08-17 11:26:08 +0200

    Set up Sentry

--- next.config.js
+++ next.config.js
@@ -3,6 +3,26 @@ const config = {
   reactStrictMode: true,
 };
 
+// React GitHub Button
 const withTM = require("next-transpile-modules")(["react-github-btn"]);
+const resultWithTM = withTM(config);
 
-module.exports = withTM(config);
+// Sentry
+const { withSentryConfig } = require("@sentry/nextjs");
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+const resultWithSentryConfig = withSentryConfig(
+  withTM(config),
+  SentryWebpackPluginOptions
+);
+
+module.exports = resultWithSentryConfig;
