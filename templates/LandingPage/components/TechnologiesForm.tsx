111790ad3dccfcd74614bf1b2234d8e97051ef75
Anders Damgaard <akd@akd.io>
2021-08-23 23:14:30 +0200

    Add support for Material UI

--- templates/LandingPage/components/TechnologiesForm.tsx
+++ templates/LandingPage/components/TechnologiesForm.tsx
@@ -66,6 +66,7 @@ const options = {
   formik: { key: "formik", value: "formik", label: "Formik" },
   prettier: { key: "prettier", value: "prettier", label: "Prettier" },
   chakra: { key: "chakra", value: "chakra", label: "Chakra UI" },
+  materialUi: { key: "materialUi", value: "material-ui", label: "Material UI" },
   framerMotion: {
     key: "framerMotion",
     value: "framer-motion",
@@ -96,7 +97,7 @@ const formStateManagementLibraries = [
   optionKeys.formik,
 ];
 const formattingLibraries = [optionKeys.prettier];
-const componentLibraries = [optionKeys.chakra];
+const componentLibraries = [optionKeys.chakra, optionKeys.materialUi];
 const animationLibraries = [optionKeys.framerMotion];
 const continuousIntegrations = [optionKeys.githubActions];
 const miscellaneousOptions = [optionKeys.formattingPreCommitHook];
@@ -158,6 +159,9 @@ export const TechnologiesForm: React.FC = () => {
     // Component Libraries
     if (formData.componentLibraries.includes(optionKeys.chakra)) {
       args.push("--chakra");
+    }
+    if (formData.componentLibraries.includes(optionKeys.materialUi)) {
+      args.push("--material-ui");
     }
 
     // Animation Libraries
@@ -374,6 +378,12 @@ export const TechnologiesForm: React.FC = () => {
                         }}
                       >
                         {options[optionKeys.chakra].label}
+                      </Checkbox>
+                      <Checkbox
+                        id={`checkbox-${optionKeys.materialUi}`}
+                        value={optionKeys.materialUi}
+                      >
+                        {options[optionKeys.materialUi].label}
                       </Checkbox>
                     </Stack>
                   </CheckboxGroup>
