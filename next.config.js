f5d265fb97287cf4ad9ed35c4cf6f435c40a88a0
Anders Kjær Damgaard <akd@akd.io>
2022-04-22 23:56:02 +0200

    Update Sentry config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+const nextTranspileModules = require("next-transpile-modules");
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
 };
 
-// React GitHub Button
-const withTM = require("next-transpile-modules")(["react-github-btn"]);
-const resultWithTM = withTM(config);
+const withTM = nextTranspileModules(["react-github-btn"]);
+
+const moduleExports = withTM(config);
 
 // Sentry
-const { withSentryConfig } = require("@sentry/nextjs");
 const SentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
@@ -20,9 +22,7 @@ const SentryWebpackPluginOptions = {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
-const resultWithSentryConfig = withSentryConfig(
-  withTM(config),
-  SentryWebpackPluginOptions
-);
 
-module.exports = resultWithSentryConfig;
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
