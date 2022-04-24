aea0af784a3e7c79cc407ad6369e8726b2a98903
Anders Kjær Damgaard <akd@akd.io>
2022-04-24 05:01:33 +0200

    Remove next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const nextTranspileModules = require("next-transpile-modules");
-
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
 };
 
-const configWithTM = nextTranspileModules(["react-github-btn"])(config);
-
-module.exports = configWithTM;
+module.exports = config;
