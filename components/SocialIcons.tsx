302b26a63ce66d5bc7a9facc023274f734b608fb
Anders Kjær Damgaard <akd@akd.io>
2022-04-25 23:11:48 +0200

    Switch to react-icons

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -1,16 +1,10 @@
-import { Flex } from "@chakra-ui/react";
-import {
-  faDiscord,
-  faGithub,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Flex, Stack } from "@chakra-ui/react";
 import React, { ComponentProps } from "react";
+import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
 
 export const SocialIcons: React.FC<ComponentProps<typeof Flex>> = (props) => {
   return (
     <Flex
-      direction="row"
       justifyContent={["left", "center"]}
       alignItems="center"
       gap="12px"
@@ -20,16 +14,16 @@ export const SocialIcons: React.FC<ComponentProps<typeof Flex>> = (props) => {
         aria-label="GitHub Repository of Create Next Stack"
         href="https://github.com/akd-io/create-next-stack"
       >
-        <FontAwesomeIcon width="24px" height="24px" icon={faGithub} />
+        <FaGithub size="24px" />
       </a>
       <a aria-label="Community Discord" href="https://discord.gg/7Ns5WwGjjZ">
-        <FontAwesomeIcon width="24px" height="24px" icon={faDiscord} />
+        <FaDiscord size="24px" />
       </a>
       <a
         aria-label="Twitter profile of the creator of Create Next Stack"
         href="https://twitter.com/akd_io"
       >
-        <FontAwesomeIcon width="24px" height="24px" icon={faTwitter} />
+        <FaTwitter size="24px" />
       </a>
     </Flex>
   );
