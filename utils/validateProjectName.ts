779c656baeab77b4d00caec035501215f63d1101
Anders Kjær Damgaard <akd@akd.io>
2022-04-27 20:46:45 +0200

    Refactor project name validation

--- utils/validateProjectName.ts
+++ utils/validateProjectName.ts
@@ -1,22 +1,23 @@
 import validateNpmPackageName from "validate-npm-package-name"
 
 /**
- * Project names must be valid NPM package names. This function checks that validity.
+ * Project names must be valid npm package names. This function checks that validity.
  *
  * @param projectName A project name
  * @returns `true` if valid. If invalid, an error message of type `string` explaining the invalidity.
  */
 export const validateProjectName = (projectName: string): string | true => {
   const problems = getProblemsInNpmPackageName(projectName)
-  return problems.length > 0 ? problems[0] : true
+  return problems[0] ?? true
 }
 
 /**
+ * Validates an npm package name.
  *
- * @param name A candidate for an NPM package name.
+ * @param npmPackageName A candidate for an npm package name.
  * @returns A list of problems found in the name.
  */
-const getProblemsInNpmPackageName = (name: string): string[] => {
-  const { errors, warnings } = validateNpmPackageName(name)
+const getProblemsInNpmPackageName = (npmPackageName: string): string[] => {
+  const { errors, warnings } = validateNpmPackageName(npmPackageName)
   return [...(errors ?? []), ...(warnings ?? [])]
 }
